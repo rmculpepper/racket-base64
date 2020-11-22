413d1f3994234136fbd87f4beb5eb7fbc03cf038
Ryan Culpepper <ryanc@racket-lang.org>
2020-11-22 20:57:47 +0100

    info

---
+++ base64-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+;; pkg info
+
+(define version "1.0")
+(define collection "base64")
+(define deps '("base"))
+(define pkg-authors '(ryanc))
+
+;; collection info
+
+(define name "base64")
